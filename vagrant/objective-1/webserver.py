4dfd90fca2d04fcac36d259038f936f3c73455df
Ellery <ellery@EllerydeMini.lan>
2018-03-29 16:41:56 +0800

    2

--- vagrant/objective-1/webserver.py
+++ vagrant/objective-1/webserver.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Create session and connect to DB
-
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -15,10 +14,11 @@ session = DBSession()
 
 
 class webServerHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         try:
-            if self.path.endswith("/restaurant"):
-                restaurant = session.query(Restaurant).all()
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
                 output = ""
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -30,22 +30,19 @@ class webServerHandler(BaseHTTPRequestHandler):
 
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
-                retur
+                return
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
 
 def main():
     try:
-        port = 8080
-        server = HTTPServer(('', port), webServerHandler)
-        print "Web Server running on port %s/restaurant in your browser" % port
+        server = HTTPServer(('', 8080), webServerHandler)
+        print 'Web server running...open localhost:8080/restaurants in your browser'
         server.serve_forever()
     except KeyboardInterrupt:
-        print " ^C entered, stopping web server...."
+        print '^C received, shutting down server'
         server.socket.close()
-
 
 if __name__ == '__main__':
     main()
