1f69084634c38ed290e8287e674903538389ead4
Ellery <ellery@EllerydeMini.lan>
2018-03-29 18:36:51 +0800

    2

--- vagrant/objective-1/webserver.py
+++ vagrant/objective-1/webserver.py
@@ -28,27 +28,27 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
                 output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
                 output += "<input type='submit' value='Create'>"
-                output += "</form></body></html>"
+                output += "</form></html></body>"
                 self.wfile.write(output)
                 return
-
             if self.path.endswith("/edit"):
                 restaurantIDPath = self.path.split("/")[2]
-                myRestaurantQuery = session.query(
-                    Restaurant).filter_by(id=restaurantIDPath).one()
-                if myRestaurantQuery != []:
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
                     self.send_response(200)
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
                     output = "<html><body>"
                     output += "<h1>"
                     output += myRestaurantQuery.name
-                    output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/%s/edit'>" % restaurantIDPath
                     output += "</h1>"
-                    output += "<input name= 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
-                    output += "<input type= 'submit' value='Rename'>"
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                    output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                    output += "<input type = 'submit' value = 'Rename'>"
                     output += "</form>"
                     output += "</body></html>"
+
                     self.wfile.write(output)
 
             if self.path.endswith("/restaurants"):
@@ -65,7 +65,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output += restaurant.name
                     output += "</br>"
                     # Objective 2 -- Add Edit and Delete Links
-                    output += "<a href ='/restaurants/%s/edit' >Edit </a> "
+                    # Objective 4 -- Replace Edit href
+                    output += "<a href ='/restaurants/%s/edit' >Edit </a> " % restaurant.id
                     output += "</br>"
                     output += "<a href =' #'> Delete </a>"
                     output += "</br></br></br>"
@@ -73,7 +74,6 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "</body></html>"
                 self.wfile.write(output)
                 return
-
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
@@ -85,46 +85,45 @@ class webServerHandler(BaseHTTPRequestHandler):
                     self.headers.getheader('content-type'))
                 if ctype == 'multipart/form-data':
                     fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('newRestaurantName')
-                restaurantIDPath = self.path.split("/")[2]
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
 
-                myRestaurantQuery = session.query(
-                    Restaurant).filter_by(id=restaurantIDPath).one()
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
 
-                if myRestaurantQuery != []:
-                    myRestaurantQuery.name = messagecontent[0]
-                    session.add(myRestaurantQuery)
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Create new Restaurant Object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
                     session.commit()
+
                     self.send_response(301)
                     self.send_header('Content-type', 'text/html')
                     self.send_header('Location', '/restaurants')
                     self.end_headers()
 
-                if self.path.endswith("/restaurants/new"):
-                    ctype, pdict = cgi.parse_header(
-                        self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('newRestaurantName')
-
-                # Create new Restaurant Object
-                newRestaurant = Restaurant(name=messagecontent[0])
-                session.add(newRestaurant)
-                session.commit()
-
-                self.send_response(301)
-                self.send_header('Content-type', 'text/html')
-                self.send_header('Location', '/restaurants')
-                self.end_headers()
-
-        except BaseException:
+        except:
             pass
 
 
 def main():
     try:
         server = HTTPServer(('', 8080), webServerHandler)
-        print 'Web server running... Open localhost:8080/restaurants in your browser'
+        print 'Web server running...open localhost:8080/restaurants in your browser'
         server.serve_forever()
     except KeyboardInterrupt:
         print '^C received, shutting down server'
